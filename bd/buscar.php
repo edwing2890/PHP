8b4cdb7c5bda292083947ec10e95f126b127f7da
Edwing <edwing_115@hotmail.com>
2015-03-14 21:27:55 -0600

    Update buscar.php

--- bd/buscar.php
+++ bd/buscar.php
@@ -1,11 +1,32 @@
 <?php
+//Se incluye la conexion con la db 
 include "conexion.php";
 
+//Si recibo el id que deseo buscar se evalua para proceder con la busqueda del registro
 if(isset($_POST['id_buscar']))
 {
-    $consulta = "SELECT * FROM practicas WHERE id=$_POST[id_buscar]";
+	//Lanzo la consulta a la tabla para obtener el registro con el id que estoy recibiendo por $_POST
+    $consulta = "SELECT * FROM registro WHERE id=$_POST[id_buscar]";
     $resultado = $conexion->query($consulta);
+	/*Para obtener el resultado de una consulta se debe aser uso de la funcion fetch la cual tiene dos variantes:
+	1.- fetch_assoc() ----> Esta funcion me permite obtener solamente un registro de una consulta, es decir para obtener los datos de un registro en 
+	base a un ID esta funcion es la que se debe utilizar
+	2.- fetch_assoc() con un ciclo while ----> Esta funcion se utiliza para obtener N cantidad de registros de una consulta, como por ejemplo
+	un listado completo de productos, empleados, etc..*/
     $datos = $resultado->fetch_assoc();
+	
+	/* El resultado del fetch_assoc() es un arreglo para el registro obtenido, por ejemplo para el registro con ID 1:
+	---------------------------------
+	| id | nombre | edad | sexo     |
+	---------------------------------
+	|  1 | Ed     | 25   | Masculino|
+	---------------------------------
+	Su resultado como arreglo seria:
+	array("id"=>1,"nombre"=>"Ed","edad"=>25,"sexo"=>"Masculino");
+	Para poder comprobar el contenido de este arreglo podemos aplicar la instruccion print_r() a la variable $datos de la sig forma:
+	print_r($datos);
+	*/
+	print_r($datos);
 }
 ?>
 <html>
@@ -13,24 +34,12 @@ if(isset($_POST['id_buscar']))
 <title>BUSCAR Y EDITAR</title>
 </head>
 <body>
-<form name="EDITAR_USU" method="POST" action="buscar.php">
+<form name="BUSCAR" method="POST" action="buscar.php">
     <h1>BUSCAR REGISTRO</h1>
     <label>Id: <input type="text" name="id_buscar" id="id_buscar" required=""/></label><br />
     <br />
     <button type="submit">BUSCAR</button>
 </form>
-
-<form name="EDITAR_USU" method="POST" action="editar.php?id=<?php echo $id;?>">
-    <h1>ACTUALIZAR DATOS</h1>
-    <label>Nombre: <input type="text" name="nombre" id="nombre" required="" value="<?php echo @$datos["nombre"];?>"/></label><br />
-    <br />
-    <label>Edad: <input type="text" name="edad" id="edad" required="" value="<?php echo @$datos["edad"];?>"/></label><br />
-    <br />
-    <label>Sexo: <input type="text" name="sexo" id="sexo" required="" value="<?php echo @$datos["sexo"];?>"/></label><br />
-    <br />
-    <input type="hidden" name="id" value="<?php echo @$datos["id"];?>">
-    <button type="submit">ACTUALIZAR</button>
-    <a href="http://www.sistemasaplicados.com.mx/index.php"><input type="button" value="CANCELAR"/> </a>
-</form>
 </body>
 </html>
+
